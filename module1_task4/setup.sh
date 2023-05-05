df2f96af8fe0b602e8131bddaf6e58417ac744f4
soniangn <nguyen.sonia@gmail.com>
2023-05-05 07:55:50 -0700

    changed version

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,7 +1,7 @@
 apt-get update && apt-get install -y hugo make
-wget https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_0.111.3_linux-amd64.tar.gz
-tar -xvf hugo_0.111.3_linux-amd64.tar.gz
-rm hugo_0.111.3_linux-amd64.tar.gz
-mv hugo_0.111.3_linux-amd64.tar.gz /usr/local/bin/
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
+tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
+mv hugo /usr/local/bin/
+rm hugo_extended_0.109.0_Linux-64bit.tar.gz
 
 make build
