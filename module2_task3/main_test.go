0c0e9ac3abbde111c6793accb44c858192b5fbb6
soniangn <nguyen.sonia@gmail.com>
2023-05-14 07:35:05 -0700

    modify test

--- module2_task3/main_test.go
+++ module2_task3/main_test.go
@@ -31,31 +31,30 @@ func Test_HelloHandler(t *testing.T) {
       body:         "Hello Rosalind Franklin!",
     },
     {
-      name:         "Sonia Nguyen",
-      queryString:  "name=Sonia Nguyen",
-      responseCode: 200,
-      body:         "Hello Sonia Nguyen!",
-    },
-    {
-      name:         "Vanessa Tessier",
-      queryString:  "name=Vanessa Tessier",
-      responseCode: 200,
-      body:         "Hello Vanessa Tessier!",
-    },
-    {
-      name:         "Mathieu Willems",
-      queryString:  "name=Mathieu Willems",
-      responseCode: 200,
-      body:         "Hello Mathieu Willems!",
-    },
-    {
-      name:         "",
+      name:         "Empty",
       queryString:  "name=",
       responseCode: 400,
-      body:         "",
     },
-    
+	{
+      name:         "B212",
+      queryString:  "name=B212",
+      responseCode: 200,
+      body:         "Hello B212!",
+    },
+	{
+		name:         "Multiple name",
+		queryString:  "name=Betty&name=Holberton",
+		responseCode: 200,
+		body:         "Hello Betty!",
+	  },
+	{
+    name:         "There",
+    queryString:  "",
+    responseCode: 200,
+    body:         "Hello there!",
+    },
   }
+
   for _, tt := range tests {
     t.Run(tt.name, func(t *testing.T) {
 
@@ -79,13 +78,15 @@ func Test_HelloHandler(t *testing.T) {
       if gotCode != expectedCode {
         t.Errorf("handler returned wrong status code: got %v want %v", gotCode, expectedCode)
       }
-
-      // Check that the response body is what you expect.
-      expectedBody := tt.body
-      gotBody := rr.Body.String()
-      if gotBody != expectedBody {
-        t.Errorf("handler returned unexpected body: got %v want %v", gotBody, expectedBody)
-      }
+	  
+	
+		// Check that the response body is what you expect.
+		expectedBody := tt.body
+		gotBody := rr.Body.String()
+		if gotBody != expectedBody {
+			t.Errorf("handler returned unexpected body: got %v want %v", gotBody, expectedBody)
+      	}	
+	  
     })
   }
 }
