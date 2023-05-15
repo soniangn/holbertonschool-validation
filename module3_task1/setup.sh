27f1490bc10bf0e527394f9b11b6250befccba14
soniangn <nguyen.sonia@gmail.com>
2023-05-15 05:22:24 -0700

    update build

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -6,4 +6,3 @@ wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0
 tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
 mv hugo /usr/local/bin/
 rm hugo_extended_0.109.0_Linux-64bit.tar.gz
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
