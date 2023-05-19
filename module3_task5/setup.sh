86d250dca2fb90ad546557db0a587befb264e83d
soniangn <nguyen.sonia@gmail.com>
2023-05-19 04:01:16 -0700

    deploy to netlify

---
+++ module3_task5/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# install a good version of hugo
+apt-get update && apt-get install -y make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
+tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
+mv hugo /usr/local/bin/
+rm hugo_extended_0.109.0_Linux-64bit.tar.gz
+npm install -g markdownlint-cli
